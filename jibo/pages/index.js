1fa28038de101ae6d411c8e29cdf6e5af4715a6b
u <agungtriutom85@gmail.com>
2024-07-11 08:04:12 +0700

    TU testing

--- jibo/pages/index.js
+++ jibo/pages/index.js
@@ -2,17 +2,17 @@ import React from "react";
 import Navigation from "../components/Navigation/Navigation";
 import Slider from "../components/HomeOne/Header";
 import About from "../components/HomeOne/Benefit";
-import Features from "../components/HomeOne/Productandsolution";
-// import Counter from "../components/HomeOne/Counter";
+// import Features from "../components/HomeOne/Productandsolution";
+import Counter from "../components/HomeOne/Counter";
 import Screenshots from "../components/Common/Documentation";
 import Testimonial from "../components/HomeOne/Footer";
-// import Pricing from "../components/HomeOne/Pricing";
-// import Faq from "../components/Common/Faq";
-// import Team from "../components/HomeOne/Team";
-// import AppDownload from "../components/HomeOne/AppDownload";
-// import Blog from "../components/HomeOne/Blog";
-// import ContactForm from "../components/Common/ContactForm";
-// import Footer from "../components/Common/Footer";
+import Pricing from "../components/HomeOne/Pricing";
+import Faq from "../components/Common/Faq";
+import Team from "../components/HomeOne/Team";
+import AppDownload from "../components/HomeOne/AppDownload";
+import Blog from "../components/HomeOne/Blog";
+import ContactForm from "../components/Common/ContactForm";
+import Footer from "../components/Common/Footer";
 
 
 const Index = () => {
@@ -27,23 +27,23 @@ const Index = () => {
 
       {/* <Features /> */}
 
-      {/* <Counter /> */}
+      <Counter />
 
-      {/* <Screenshots />
+      <Screenshots />
 
-      <Testimonial /> */}
+      <Testimonial />
 
-      {/* <Pricing /> */}
+      <Pricing />
 
-      {/* <Faq /> */}
+      <Faq />
 
-      {/* <Team /> */}
+      <Team />
 
-      {/* <AppDownload /> */}
+      <AppDownload />
 
-      {/* <Blog /> */}
+      <Blog />
 
-      {/* <ContactForm /> */}
+      <ContactForm />
 
       {/* <Footer /> */}
     </>
