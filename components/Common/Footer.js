1a29b0959116b69a52c2963c87b30fa7648dc0a3
TU <agungtriutomo85@gmail.com>
2024-08-12 09:17:59 +0700

    update

--- components/Common/Footer.js
+++ components/Common/Footer.js
@@ -5,43 +5,58 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
     <>
-      <footer id="footer">
+      <Footer id="footer">
         <NewsletterForm />
 
-        <div className="footer-bottom" style={{backgroundColor: '#c19a6b'}}>
+        <div className="footer-bottom" style={{ backgroundColor: "#c19a6b" }}>
           <div className="container">
             <div className="row">
               <div className="col-md-6 col-lg-6">
-                <p className="copyright" style={{color:'#FFFF'}}>
+                <p className="copyright" style={{ color: "#FFFF" }}>
                   Copyright {currentYear} Jibo. all rights by{" "}
-                  <a href="http://envytheme.com/" target="_blank" style={{color:'#FFFF'}}>
+                  <a
+                    href="http://envytheme.com/"
+                    target="_blank"
+                    style={{ color: "#FFFF" }}
+                  >
                     EnvyTheme
-                    
                   </a>
                 </p>
               </div>
 
               <div className="col-md-6 col-lg-6">
-                <ul className="social-links"  >
-                  <li style={{color:'#FFFF'}}>Follow us :</li>
-                  <li >
-                    <a href="https://facebook.com/" target="_blank" >
-                      <i className="icofont-facebook" style={{color:'#FFFF'}}></i>
+                <ul className="social-links">
+                  <li style={{ color: "#FFFF" }}>Follow us :</li>
+                  <li>
+                    <a href="https://facebook.com/" target="_blank">
+                      <i
+                        className="icofont-facebook"
+                        style={{ color: "#FFFF" }}
+                      ></i>
                     </a>
                   </li>
                   <li>
                     <a href="https://twitter.com/" target="_blank">
-                      <i className="icofont-twitter"  style={{color:'#FFFF'}}></i>
+                      <i
+                        className="icofont-twitter"
+                        style={{ color: "#FFFF" }}
+                      ></i>
                     </a>
                   </li>
                   <li>
                     <a href="https://www.instagram.com/" target="_blank">
-                      <i className="icofont-instagram"  style={{color:'#FFFF'}}></i>
+                      <i
+                        className="icofont-instagram"
+                        style={{ color: "#FFFF" }}
+                      ></i>
                     </a>
                   </li>
                   <li>
                     <a href="https://www.pinterest.com/" target="_blank">
-                      <i className="icofont-pinterest" style={{color:'#FFFF'}}></i>
+                      <i
+                        className="icofont-pinterest"
+                        style={{ color: "#FFFF" }}
+                      ></i>
                     </a>
                   </li>
                 </ul>
@@ -49,7 +64,7 @@ const Footer = () => {
             </div>
           </div>
         </div>
-      </footer>
+      </Footer>
     </>
   );
 };
