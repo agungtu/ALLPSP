688ee91affc4b612be27ee1d1e8d9cbadf9ec053
fauzi <->
2024-08-12 08:31:08 +0700

    Update

--- components/Common/Footer.js
+++ components/Common/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <>
       <footer id="footer">
-        {/* NewsletterForm */}
+
         <NewsletterForm />
 
         <div className="footer-bottom" style={{backgroundColor: '#c19a6b'}}>
