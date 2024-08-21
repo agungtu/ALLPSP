5ad1909356213d7e6a3190035f4e285e51b6c171
unknown <megadewikp@gmail.com>
2024-08-21 15:50:44 +0700

    update

--- pages/august-week-3/ToDo/index.js
+++ pages/august-week-3/ToDo/index.js
@@ -54,48 +54,66 @@ const Branch = () => {
         </div>
       </section>
 
-      <section className="content-section" style={{backgroundColor : "#FEFCF3"}}>
+      <section className="content-section" style={{ backgroundColor: "#FEFCF3" }}>
         <div className="container"
-            data-aos="zoom-in"
-            data-aos-duration="800"
-            data-aos-delay="100"
+          data-aos="zoom-in"
+          data-aos-duration="800"
+          data-aos-delay="100"
         >
-          <h2 style={{ textAlign: "center",  color : "#B06161", backgroundColor: "#FFF5E4"}}> Profil</h2>
-          <button onClick={() => getData()}>Profil</button>
-          {userData && (
-            <ul>
-              <li>Id: {userData.id}</li>
-              <li>
-                Name: {userData.firstName} {userData.lastName}
-              </li>
-              <li>Email: {userData.email}</li>
-              <li>Phone: {userData.phone}</li>
-              <li>birthDate: {userData.birthDate}</li>
-            </ul>
-          )}
 
-          
-            <h2 style={{ textAlign: "center",  color : "#B06161" , backgroundColor: "#FFF5E4"}}>My To Do List</h2>
-          <div className="content">
-            <button onClick={() => getAllData()}>Show All</button>
-            <table>
-              {todosData.map((item) => {
-                return (
-                  <tr key={item}>
-                    <td style={{
-                      textDecoration: item.completed ? 'line-through' : 'none',
-                      color: item.completed ? 'green' : 'red'
-                    }}>
-                      {item.todo}
-                    </td>
+          <h2 style={{ textAlign: "center", color: "#B06161" }}> Profil</h2>
+          <div className="container ">
+            <div class="row">
+              <div class="col">
+                <img width={260} style={{ borderRadius: "25px" }} src="/images/hoshii.jpg"></img>
+              </div>
+              <div className="col">
+                <div class="border border-success p-2 mb-2 border-opacity-50" style={{ backgroundColor: "#FFFF" }}>
 
-                  </tr>
-                );
-              })}
-            </table>
+                  <b style={{ color: "#EF9C66" }}>
+                    <dt>Id: {userData.id}</dt>
+                    <dt>
+                      Name: {userData.firstName} {userData.lastName}
+                    </dt>
+                    <dt>Email: {userData.email}</dt>
+                    <dt>Phone: {userData.phone}</dt>
+                    <dt>birthDate: {userData.birthDate}</dt>
+                  </b>
+
+                  <button class="btn btn-warning" onClick={() => getData()} style={{ color: "#FFFF" }}>Profil</button>
+                </div>
+              </div>
+            </div>
           </div>
+
+
+          <h2 style={{ textAlign: "center", color: "#B06161" }}>Daftar Tugas Saya</h2>
+          <div class="container">
+            <div class="row ">
+              <div class="col">
+                <button class="btn btn-warning" onClick={() => getAllData()} style={{ color: "#FFFF" }}>Show All</button>
+              </div>
+              <div class="col">
+                <table className="border border-success p-2 mb-2 border-opacity-50" >
+                  {todosData.map((item) => {
+                    return (
+                      <tr key={item}>
+                        <b style={{
+                          textDecoration: item.completed ? 'line-through' : 'none',
+                          color: item.completed ? 'green' : 'red'
+                        }}>
+                          {item.todo}
+                        </b>
+
+                      </tr>
+                    );
+                  })}
+                </table>
+              </div>
+            </div>
           </div>
-        
+
+        </div>
       </section>
 
       <Footer />
